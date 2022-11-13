5f815b71c693a184ba0ced4a031c77c11afa75b5
ypranab <ypranab11@gmail.com>
2022-11-13 20:27:52 +0600

    style added

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -9,9 +9,9 @@ const Activities = (props) => {
         <div className='activity-container'>
             <div className='activities'>
                 <img className='img-activity' src={picture} alt="" />
-                <p>{name}</p>
-                <p>{age}</p>
-                <p>{activityTime}</p>
+                <h2>{name}</h2>
+                <p>For Age: {age}</p>
+                <p>Time: {activityTime}s</p>
             </div>
             <button onClick={() => addToList(activity)} className='btn-list'>
                 <p>Add to list</p>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -15,25 +15,31 @@ const Profile = ({ activeList }) => {
 
     return (
         <div className='profile-summary'>
-            <h2>This is Profile</h2>
+            <h2>Profile Summary</h2>
             <div className='profile-section'>
                 <img className='profile-img' src={kevin} alt="" />
                 <h3>Jahid Hasan</h3>
             </div>
-            <div>
-                <span>75kg</span>
-                <span>6.5feet</span>
-                <span>25years</span>
+            <div className='details-div'>
+                <div>
+                    <span className='details'>75kg</span>
+                    <span className='details'>6.5feet</span>
+                    <span className='details'>25years</span>
+                </div>
+                <span className='details'>Weight</span>
+                <span className='details'>Height</span>
+                <span className='details'>Age</span>
             </div>
-            <h3>add a break</h3>
-            <div>
+            <h3>Add a Break</h3>
+            <div className='btn-div'>
                 <button className='btn-break' onClick={() => addToBreak(20)}>20s</button>
                 <button className='btn-break' onClick={() => addToBreak(30)}>30s</button>
                 <button className='btn-break' onClick={() => addToBreak(40)}>40s</button>
                 <button className='btn-break' onClick={() => addToBreak(50)}>50s</button>
             </div>
-            <p>Time:{activeTimer}</p>
-            <p>Break Time:{breakTime}</p>
+            <h2>Exercise Details</h2>
+            <p className='details-div'>Exercise Time: {activeTimer} Seconds</p>
+            <p className='details-div'>Break Time: {breakTime} Seconds</p>
         </div>
     );
 };
